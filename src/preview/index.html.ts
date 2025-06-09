5bb60dfb93bc835751ec6edfccea4d12c2b9bec4
Hovhannes Sahakyan <hovhannes@superannotate.com>
2025-06-09 17:31:32 +0200

    feat: add design

--- src/preview/index.html.ts
+++ src/preview/index.html.ts
@@ -24,9 +24,16 @@ function script() {
     }
 
     function update({ value: { active: { selectors, declarations }, docText } }: UpdateMessage) {
-        window.document.getElementById(USER_STYLES)!.innerText = `#${PREVIEW} { ${docText} }`;
-        window.document.getElementById(USER_SELECTORS)!.innerText = docText.substring(selectors[0], selectors[1]);
-        window.document.getElementById(ACTIVE_STYLES)!.innerText = `#${TARGET} ${docText.substring(declarations[0], declarations[1])}`;
+        window.document.getElementById(USER_STYLES)!.textContent = `#${PREVIEW} { ${docText} }`;
+        window.document.getElementById(USER_SELECTORS)!.replaceChildren(
+            ...docText.substring(selectors[0], selectors[1]).split(',').map((s) => {
+                const node = document.createElement('span');
+                node.className = 'selector';
+                node.textContent = s.trim();
+                return node;
+            })
+        );
+        window.document.getElementById(ACTIVE_STYLES)!.textContent = `#${TARGET} ${docText.substring(declarations[0], declarations[1])}`;
     }
 
     window.addEventListener('message', (e) => {
@@ -46,12 +53,52 @@ export default `
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <style>
+    body {
+        padding: unset;
+        font-family: "Roboto", sans-serif;
+    }
+    .heading {
+        margin: 0;
+        padding: 20px;
+        font-size: 16px;
+        font-weight: 400;
+        border-bottom: 1px solid currentColor;
+    }
+    .selectors-container {
+        display: flex;
+        align-items: center;
+        padding: 16px;
+        gap: 10px;
+        border-bottom: 1px solid currentColor;
+
+        .selectors-container__label {
+            font-size: 14px;
+        }
+
+        #selectors {
+            display: contents;
+        }
+
+        .selector {
+            padding: 6px 16px;
+            border: 1px solid currentColor;
+            border-radius: 20px;
+        }
+    }
+    </style>
     <style id="user-styles"></style>
     <style id="active-styles"></style>
 </head>
 <body>
-    <h1>CSS preview</h1>
-    <div id="selectors"></div>
+    <h1 class="heading">CSS preview</h1>
+    <div class="selectors-container">
+        <span class="selectors-container__label">Selectors: </span>
+        <div id="selectors"></div>
+    </div>
     <div id="preview">
         <div id="target">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur omnis reiciendis sint! Nulla harum, exercitationem asperiores suscipit maxime delectus mollitia, sint quaerat vel doloribus laudantium porro assumenda, quae nostrum dolores.
